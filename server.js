39269a6616213f2a696746acc8d8f16398e552fe
Bendalam Saiteja <52485248+saiteja6@users.noreply.github.com>
2019-08-19 16:07:35 +0530

    updated file

--- server.js
+++ server.js
@@ -1,53 +1,61 @@
 const express = require('express')
-const mongoose = require('mongoose');
-const News = require('./Model/news')
-const app = express()
-const port = 3000;
-
+const mongoose = require('mongoose')
+const app = express();
+const news = require('./Model/news')
+const db = require('./config/keys').db;
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 app.use(express.urlencoded({
     extended : true
 }))
-
 app.use(express.json())
 
 //model
 
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
     console.log('hello');
-})
+})  
 
-
+  
 app.get('/getnews', (req, res) => {
     
+    news.find().then(post=>{
+        if(!post){
+           return res.status(404).json({
+               nopost:"no post found"
+
+            })
+             
+        }
+        res.json(post)
     
-    res.send('Hello world!');
-    console.log('hello');
+    })
+    
+    //console.log('Hello');
+    
 })
 
+mongoose.connect(db,{useNewUrlParser:true},() => {
 
-mongoose.connect('mongodb+srv://sai:saiteja@hackernews-8kxm3.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser : true},() => {
+     
     console.log('database connected');
 });
 
 app.post('/register', (req, res) => {
-    // console.log("fndksjfn", News)
-    const {title, url, id} = req.body;
-    // console.log("fdsfs", req.body)
-    const article = new News({
+    const {title,url} = req.body;
+    const news = new News({
         title,
-        url,
-        id
-    }).save()
+        url
+})     
+
+.save()
     .then(news => {
-        // console.log("news");
         res.json(news)
     })
     .catch(err => {
-        // console.log("erere", err)
         res.json(err)
     })
 });
-
+const port =3000;
 app.listen(port)
